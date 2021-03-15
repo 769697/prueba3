57841070cc380a8b390b877e9a4d2fdfebf49ecf
769697 <769697@unizar.es>
2021-03-15 21:26:30 +0100

    primer commit mis pana

---
+++ prueba3/src/main/java/Modelo/Bus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author minguez
+ */
+public class Bus {
+    private int num1;
+    private int num2;
+    
+    public void sumar(int n1, int n2){
+        int resultado = n1 +n2;
+        System.out.println("EL resultado es: "+ resultado);
+    }
+    
+}
